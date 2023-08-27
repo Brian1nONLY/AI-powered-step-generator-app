251790dc30923d61be39424d03f979103bbbd490
Yephihy <offianoura@gmail.com>
2023-08-27 07:14:06 -0400

    styles again

--- client/src/App.js
+++ client/src/App.js
@@ -13,15 +13,18 @@ function App() {
       <img src = {Logo} className="h-36"></img>
       </div>
       <Heading1 
-      content = "So, What are we doing today?" size = "3xl">
+      content = "So, What are we doing today?" fontSize = "3xl">
       </Heading1>
-  
+
+      <form className="flex flex-col gap-y-4">
+
+     
       <InputField></InputField>
       <div className="flex flex-row justify-center">
-      <button className="text-3xl h-11 w-8/12 bg-zinc-800 rounded-3xl font-h1 text-white">Let's plan this!</button>
-    
-      </div>
+      <button className="text-3xl h-11 w-8/12 bg-zinc-800 rounded-3xl font-h1 text-white" type ="submit">Let's plan this!</button>
       
+      </div>
+      </form>
     </div>
   );
 }
