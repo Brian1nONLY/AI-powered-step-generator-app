a87de8f2c2fdb64ba07aab6ca93795c55b273429
urmaysuthar <urmay.suthar0100@gmail.com>
2023-08-27 10:40:57 -0400

    Updated App

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,105 @@ import InputField from "./Components/InputField";
 
 import Heading1 from "./Components/Heading1";
 function App() {
- 
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+
+    fetch("http://localhost:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chats,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        msgs.push(data.output);
+        setChats(msgs);
+        setIsTyping(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-   <div className="flex  gap-y-10 flex-col align-center justify-center">
-      <div className="flex flex-row justify-center">
-      <img src = {Logo} className="h-36"></img>
+    <main className="p-4 flex flex-col items-center bg-white">
+      <img src={Logo} className="h-36"></img>
+
+      <div className="flex items-center justify-center mt-4">
+        <Heading1 content="What do you want to plan?" size="6xl" />
       </div>
-      <Heading1 
-      content = "So, What are we doing today?" size = "3xl">
-      </Heading1>
-  
-      <InputField></InputField>
-      <div className="flex flex-row justify-center">
-      <button className="text-3xl h-11 w-8/12 bg-gray-800 rounded-3xl font-h1 text-white">Let's plan this!</button>
-    
-      </div>
+
+      <section className="w-full mt-4 w-9/12">
+        {chats && chats.length ? (
+          chats.map((chat, index) => (
+            <p
+              key={index}
+              className={`${
+                chat.role === "user" ? "bg-gray-100" : "bg-gray-300"
+              } p-2 rounded`}
+            >
+              <span>
+                <b>{chat.role.toUpperCase()}</b>
+              </span>
+              <span>: </span>
+              <span>{chat.content}</span>
+            </p>
+          ))
+        ) : (
+          <p>Nothing planned so far!</p>
+        )}
+      </section>
       
-    </div>
+      <div className="flex items-center justify-center mt-4">
+        </div>
+      <div className="bg-gray-200 p-4 w-9/12">
+        <form
+          onSubmit={(e) => chat(e, message)}
+          className="flex items-center justify-center"
+        >
+          <input
+            type="text"
+            name="message"
+            value={message}
+            placeholder="Type a message here and hit Enter..."
+            onChange={(e) => setMessage(e.target.value)}
+            className="border border-gray-300 rounded p-2 w-9/12"
+          />
+          <button
+            type="submit"
+            className="bg-black text-white px-4 py-2 ml-2 rounded w-1/6"
+          >
+            Lets plan this!
+          </button>
+        </form>
+      </div>
+
+      <div
+        className={`flex items-center justify-center mt-4 ${
+          isTyping ? "" : "hidden"
+        }`}
+      >
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
+    </main>
   );
 }
 
--- client/src/Components/InputField.js
+++ client/src/Components/InputField.js
@@ -1,8 +1,8 @@
 function InputField() {
  
     return (
-     <div className="flex flex-row align-center justify-center">
-       <input className="bg-slate-200 h-64 w-10/12 rounded-3xl">
+     <div className="flex flex-row align-auto justify-center">
+       <input className="bg-slate-200 h-64 w-4/5 rounded-3xl">
        
        </input> 
       </div>
